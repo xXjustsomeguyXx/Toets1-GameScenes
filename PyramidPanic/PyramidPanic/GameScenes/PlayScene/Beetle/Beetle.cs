6ac4240baa5030a7c9b0b4731134b1c8640a8aba
xXjustsomeguyXx <roy97@casema.nl>
2014-01-28 18:27:16 +0100

    Commentaar van de Beetle class gedaan

--- PyramidPanic/PyramidPanic/GameScenes/PlayScene/Beetle/Beetle.cs
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/Beetle/Beetle.cs
@@ -14,7 +14,7 @@ namespace PyramidPanic
 {
     public class Beetle : IAnimatedSprite
     {
-        //Fields
+        //Fields methode van de class Beetle
         private PyramidPanic game;
         private IEntityState state;
         private Texture2D texture;
@@ -25,55 +25,64 @@ namespace PyramidPanic
         private WalkUp walkUp;
         private WalkDown walkDown;
 
-        //properties
+        //properties methode van de beetle class
         public WalkUp WalkUp
         {
+            // Getter van Walkupclass
             get { return this.walkUp; }
         }
         public WalkDown WalkDown
         {
+            // Getter van WalkDownclass
             get { return this.walkDown; }
         }
         public Vector2 Position
         {
+            // Getter en Setter van de field Vector2
             get { return this.position; }
             set { this.position = value; }
         }
         public IEntityState State
         {
+            // Setter van de IEntityState
             set { this.state = value; }
         }
         public PyramidPanic Game
         {
+            // Getter van de Pyramidpanic Game (Hoofdclass)
             get { return this.game; }
         }
         public int Speed
         {
+            // Getter van de field Speed om te bepalen hoesnel de beetle kan.
             get { return this.speed; }
         }
         public Texture2D Texture
         {
+            // Getter van de field Texture2D 
             get { return this.texture; }
         }
         
-        //Constructor
+        //Constructor methode van de Beetle class
         public Beetle(PyramidPanic game, Vector2 position)
         {
             this.game = game;
             this.position = position;
             this.texture = game.Content.Load<Texture2D>(@"Beetle\Beetle");
+            // De Constructor van de class Walkup
             this.walkUp = new WalkUp(this);
+            // De Constructor van de class WalkDown
             this.walkDown = new WalkDown(this);
             this.state = this.walkUp;
         }
 
-        //Update
+        //Update methode van de beetle class
         public void Update(GameTime gameTime)
         {
            this.state.Update(gameTime);
         }
 
-        //Draw
+        //Draw methode van de Beetle class
         public void Draw(GameTime gameTime)
         {
             this.state.Draw(gameTime);                   
